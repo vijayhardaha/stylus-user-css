4e6d18a66d50514487c86e687f62bcaf45f690e5
vijayhardaha <vijayhardaha@gmail.com>
2023-10-29 11:02:40 +0530

    fix(gulp): fix watch errors

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -6,7 +6,6 @@ import autoprefixer from "autoprefixer"; // Autoprefix CSS properties
 import cleancss from "gulp-clean-css"; // Minify CSS
 import dartSass from "sass"; // Compile SCSS to CSS
 import duplicates from "postcss-discard-duplicates"; // Remove duplicate CSS rules
-import filter from "gulp-filter"; // Filter files
 import gulp from "gulp"; // Gulp task runner
 import gulpSass from "gulp-sass"; // Compile SCSS to CSS
 import plumber from "gulp-plumber"; // Handle errors gracefully
@@ -16,11 +15,6 @@ import rename from "gulp-rename"; // Rename files
 const sass = gulpSass(dartSass);
 
 /**
- * Exclude files with an underscore prefix using gulp-filter.
- */
-const excludeUnderscoredFiles = filter((file) => !/\/_/.test(file.path));
-
-/**
  * Build CSS from LESS files.
  *
  * @param {Function} done - A callback function to signal task completion.
@@ -28,9 +22,8 @@ const excludeUnderscoredFiles = filter((file) => !/\/_/.test(file.path));
 const buildCSS = (done) => {
 	gulp
 		.src("src/**/*.scss") // Source LESS files
-		.pipe(excludeUnderscoredFiles) // Exclude files starting with an underscore
 		.pipe(plumber()) // Handle errors gracefully
-		.pipe(sass({ quietDeps: true })) // Compile SCSS to CSS
+		.pipe(sass()) // Compile SCSS to CSS
 		.pipe(postcss([duplicates(), autoprefixer()])) // Process CSS with PostCSS plugins
 		.pipe(cleancss({ format: "beautify" })) // Minify CSS
 		.pipe(rename({ suffix: ".user" })) // Rename output files
@@ -56,7 +49,7 @@ const cleanAssets = (done) => {
  * @param {Function} done - A callback function to signal task completion.
  */
 const watchAssets = (done) => {
-	gulp.watch("src/**/*.scss", buildCSS); // Watch LESS files for changes and run 'buildCSS' task
+	gulp.watch("src/**/*.scss", gulp.series(buildCSS)); // Watch LESS files for changes and run 'buildCSS' task
 
 	done(); // Signal task completion
 };
@@ -65,4 +58,5 @@ const watchAssets = (done) => {
 const build = gulp.series(cleanAssets, buildCSS);
 
 // Export the 'build' task and rename 'watchAssets' to 'watch' for better clarity.
-export { build, watchAssets as watch };
+export { build };
+export { watchAssets as watch };
